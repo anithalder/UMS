90a22a2c6944f11375155b06d4f639023924bb88
Halka_Coding <87062016+anithalder@users.noreply.github.com>
2026-01-31 20:05:45 +0530

    Backend project setup

---
+++ ManagementSystem/CUManagement/src/main/java/com/example/CUManagement/CuManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.CUManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuManagementApplication.class, args);
+	}
+
+}
---
+++ ManagementSystem/CUManagement/src/test/java/com/example/CUManagement/CuManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CUManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
